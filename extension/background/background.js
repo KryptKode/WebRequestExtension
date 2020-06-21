baa019778b8e9c46e27fbdd30d2d105becef8912
kryptkode <paulakhamiogu@gmail.com>
2020-06-21 23:25:21 +0100

    Added loading indicator

--- extension/background/background.js
+++ extension/background/background.js
@@ -17,9 +17,15 @@ chrome.runtime.onInstalled.addListener(function () {
 
     chrome.webRequest.onSendHeaders.addListener(
       function (details) {
+        console.log("HEADER DETAILS: ", details);
+        chrome.storage.sync.get(["addItemBody", "authToken"], function (data) {
+          console.log(`ITEM: ${data.addItemBody} --- TOKEN: ${data.authToken}`);
+        });
         const authToken = getHeader(details.requestHeaders, tokenHeaderName);
         console.log("Headers: ", authToken);
-        saveItem("authToken", authToken);
+        if (authToken && authToken != "null") {
+          saveItem("authToken", authToken);
+        }
       },
       { urls: [`https://${woolworths}/*`, `http://${woolworths}/*`] },
       ["requestHeaders", "extraHeaders"]
@@ -27,6 +33,10 @@ chrome.runtime.onInstalled.addListener(function () {
 
     chrome.webRequest.onBeforeRequest.addListener(
       function (details) {
+        console.log("details--> ", details);
+        chrome.storage.sync.get(["addItemBody", "authToken"], function (data) {
+          console.log(`ITEM:`, data);
+        });
         const rawBody = details.requestBody.raw;
         if (rawBody.length > 0) {
           const body = ab2str(rawBody[0].bytes);
@@ -43,11 +53,101 @@ chrome.runtime.onInstalled.addListener(function () {
       ["requestBody"]
     );
   });
+
+  var url = "https://www.woolworths.com.au/apis/ui/Trolley/AddItems";
+  chrome.runtime.onConnect.addListener(function (port) {
+    if ((port.name = "addList")) {
+      port.onMessage.addListener(function (message) {
+        if (message.action == "addList") {
+          port.postMessage({ loading: true });
+          chrome.storage.sync.get(["addItemBody", "authToken"], function (
+            data
+          ) {
+            var body = data.addItemBody;
+            var token = data.authToken;
+            if (body && token) {
+              postData(url, body, token)
+                .then((result) => {
+                  if (result.status == 200) {
+                    port.postMessage({
+                      loading: false,
+                      success: true,
+                      message: "List added successfully",
+                    });
+                  } else {
+                    if (result.status == 400) {
+                      port.postMessage({
+                        loading: false,
+                        success: false,
+                        message:
+                          "Could not add the list to this account.\nCould be that the list is already added or you may need to log out and login",
+                      });
+                    } else {
+                      port.postMessage({
+                        loading: false,
+                        success: false,
+                        message: "Could not add the list. Please try again",
+                      });
+                    }
+                  }
+                  console.log("RESULT ", result);
+                })
+                .catch((err) => {
+                  port.postMessage({
+                    loading: false,
+                    success: false,
+                    message: "An error occurred. Please try again",
+                  });
+                  console.error("An error ocurred", err);
+                });
+            } else {
+              const message = body
+                ? "Cannot detect current account, please perform some actions on the page"
+                : "No products";
+              console.log(message);
+              port.postMessage({
+                loading: false,
+                success: false,
+                message: message,
+              });
+            }
+          });
+        }
+      });
+    }
+  });
 });
 
+async function postData(url = "", data, token = "") {
+  console.log("URL: ", url);
+  console.log("DATA: ", data);
+  console.log("TOKEN: ", token);
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      __requestverificationtoken: token,
+      origin: "https://www.woolworths.com.au",
+      referrer: "https://www.woolworths.com.au/shop/cart",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: data, // body data type must match "Content-Type" header
+  });
+  console.log("SERVER", response);
+  return response;
+}
+
 function saveItem(key, value) {
-  chrome.storage.sync.set({ key: value }, function () {
+  var obj = {};
+  obj[key] = value;
+  chrome.storage.sync.set(obj, function () {
     console.log(`Saved item: KEY ${key}, VALUE: ${value}`);
+    chrome.storage.sync.get([key], function (data) {
+      console.log("GET", data);
+    });
   });
 }
 
--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -2,15 +2,55 @@ var addButton = document.getElementById("add-list");
 
 var viewButton = document.getElementById("view-list");
 
+var errorText = document.getElementById("error-text");
+var successText = document.getElementById("success-text");
+
+var loadingIndicator = document.getElementById("loader");
+var listContent = document.getElementById("list-content");
+var showingList = false;
+
+var port = chrome.runtime.connect({ name: "addList" });
+port.onMessage.addListener(function (msg) {
+  handleResponse(msg);
+});
+
 addButton.addEventListener("click", function () {
   console.log("Add button clicked");
+  chrome.storage.sync.get(["addItemBody", "authToken"], function (data) {
+    var body = data.addItemBody;
+    var token = data.authToken;
+    if (body && token) {
+      port.postMessage({ action: "addList" });
+    } else {
+      const message = body
+        ? "Cannot detect current account, please perform some actions on the page"
+        : "No products";
+      showError(message);
+    }
+  });
 });
 
 viewButton.addEventListener("click", function () {
-  console.log("View button clicked");
+  if (showingList) {
+    hideList();
+    showingList = false;
+  } else {
+    showList();
+    showingList = true;
+  }
 });
 
-function listExists() {}
+function handleResponse(response) {
+  if (response) {
+    if (response.loading) {
+      showLoading();
+    } else {
+      var message = response.message;
+      hideLoading();
+      showError(message);
+    }
+  }
+}
 
 function enableOrDisableAddButton() {
   chrome.storage.sync.get(["addItemBody", "authToken"], function (data) {
@@ -19,8 +59,11 @@ function enableOrDisableAddButton() {
       enabled = true;
     }
     addButton.disabled = !enabled;
+    if (!enabled) {
+      addButton.className += " btn-disabled";
+      addButton.parentNode.className += " flex-disabled";
+    }
     console.log("Add button is enabed", enabled);
-    console.log("Add button is enabed data", data);
   });
 }
 
@@ -28,11 +71,107 @@ function enableOrDisableViewButton() {
   chrome.storage.sync.get("addItemBody", function (data) {
     var enabled = data.addItemBody;
     viewButton.disabled = !enabled;
+    if (!enabled) {
+      viewButton.className += " btn-disabled";
+      viewButton.parentNode.className += " flex-disabled";
+    }
     console.log("View button is enabed", enabled);
   });
 }
 
-function addItemHttpRequest() {}
+async function postData(url = "", data, token = "") {
+  console.log("URL: ", url);
+  console.log("DATA: ", data);
+  console.log("TOKEN: ", token);
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      __requestverificationtoken: token,
+      origin: "https://www.woolworths.com.au",
+      referrer: "https://www.woolworths.com.au/shop/cart",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: data, // body data type must match "Content-Type" header
+  });
+  console.log("SERVER", response);
+  return response;
+}
+
+function showError(message) {
+  errorText.style.display = "block";
+  errorText.innerHTML = message;
+}
+
+function hideError() {
+  errorText.style.display = "none";
+}
+
+function showSuccess(message) {
+  successText.style.display = "block";
+  successText.innerHTML = message;
+}
+
+function hideSuccess() {
+  successText.style.display = "none";
+}
+function showLoading() {
+  loadingIndicator.style.display = "flex";
+}
+
+function hideLoading() {
+  loadingIndicator.style.display = "none";
+}
+
+function showList() {
+  chrome.storage.sync.get(["addItemBody"], function (data) {
+    var body = data.addItemBody;
+    if (body) {
+      viewButton.innerHTML = "Hide List";
+      listContent.style.display = "block";
+      listContent.innerHTML = syntaxHighlight(
+        JSON.stringify(JSON.parse(body), undefined, 4)
+      );
+      showingList = true;
+    } else {
+      showingList = false;
+      showError("No list found");
+    }
+  });
+}
+
+function hideList() {
+  listContent.style.display = "none";
+  viewButton.innerHTML = "View List";
+}
+
+function syntaxHighlight(json) {
+  json = json
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
+  return json.replace(
+    /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+    function (match) {
+      var cls = "number";
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = "key";
+        } else {
+          cls = "string";
+        }
+      } else if (/true|false/.test(match)) {
+        cls = "boolean";
+      } else if (/null/.test(match)) {
+        cls = "null";
+      }
+      return '<span class="' + cls + '">' + match + "</span>";
+    }
+  );
+}
 
 enableOrDisableAddButton();
 enableOrDisableViewButton();
